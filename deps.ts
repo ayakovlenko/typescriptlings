d2c6657067acb54a990339ca3708477104c3afdb
tony <ayakovlenko@users.noreply.github.com>
2021-06-24 22:44:46 +0300

    chore: upgrade dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,8 @@
-import { normalize, relative } from "https://deno.land/std@0.92.0/path/mod.ts";
+import { normalize, relative } from "https://deno.land/std@0.99.0/path/mod.ts";
+export { normalize, relative };
 
-import { isWindows } from "https://deno.land/std@0.92.0/_util/os.ts";
+import { isWindows } from "https://deno.land/std@0.99.0/_util/os.ts";
+export { isWindows };
 
-import * as Colors from "https://deno.land/std@0.92.0/fmt/colors.ts";
-
-export { Colors, isWindows, normalize, relative };
+import * as Colors from "https://deno.land/std@0.99.0/fmt/colors.ts";
+export { Colors };
