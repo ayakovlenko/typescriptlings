832941d35c6c28e8259bddef09bd5f98abeb6165
tony <ayakovlenko@users.noreply.github.com>
2022-12-02 20:13:41 +0200

    refactor: file system events (#12)

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,72 @@
 import { check, isDone } from "./runner.ts";
 import { exercises } from "../exercises/index.ts";
 import { State } from "./state.ts";
-import { relative } from "./deps.ts";
 import * as ui from "./ui.ts";
+import { FsEvent, FsEventObservable, Observer } from "./fs.ts";
 
-const FileModifiedEvent = "file-modified";
-
-const state = new State(exercises);
-
-// rewind to the next uncompleted exercise
-const rewind = async () => {
-  let exercise = state.current();
-  while (exercise && await check(exercise) && await isDone(exercise)) {
-    ui.successfulRun(exercise);
-    exercise = state.next();
-  }
+type TypeScriptlingsOpts = {
+  state: State;
 };
 
-const trigger = () => {
-  const exercise = state.current();
-  if (exercise) {
-    window.dispatchEvent(new Event(FileModifiedEvent));
-  } else {
-    ui.congratsAndExit();
+class TypeScriptlings implements Observer<FsEvent> {
+  private readonly state: State;
+
+  constructor({ state }: TypeScriptlingsOpts) {
+    this.state = state;
   }
-};
 
-window.addEventListener(FileModifiedEvent, async () => {
-  const exercise = state.current();
-  if (exercise) {
-    const { ok, output } = await check(exercise);
-    const done = await isDone(exercise);
-
-    if (ok && done) {
-      state.next();
-      await rewind();
-      trigger();
-    } else if (ok && !done) {
-      ui.successfulRun(exercise);
-      console.log("\n" + output);
-      ui.nextInstuctions();
-    } else {
-      // !ok && done
-      // !ok && !done
-      ui.failedRun(exercise);
-      console.log(output);
+  update({ kind }: FsEvent): void | PromiseLike<void> {
+    switch (kind) {
+      case "modify": {
+        this.checkExercise();
+      }
     }
   }
+
+  async checkExercise() {
+    const exercise = this.state.current();
+    if (exercise) {
+      const { ok, output } = await check(exercise);
+      const done = await isDone(exercise);
+
+      if (ok && done) {
+        // trigger next exercise
+        await this.rewind();
+        this.checkExercise();
+      } else if (ok && !done) {
+        ui.successfulRun(exercise);
+        console.log("\n" + output);
+        ui.nextInstuctions();
+      } else {
+        // !ok && done
+        // !ok && !done
+        ui.failedRun(exercise);
+        console.log(output);
+      }
+    } else {
+      ui.congratsAndExit();
+    }
+  }
+
+  async rewind() {
+    let exercise = this.state.current();
+    while (exercise && await check(exercise) && await isDone(exercise)) {
+      ui.successfulRun(exercise);
+      exercise = this.state.next();
+    }
+  }
+}
+
+const tslings = new TypeScriptlings({
+  state: new State(exercises),
 });
 
-await rewind();
-await trigger();
+new FsEventObservable({
+  watch: "./exercises",
+  observer: tslings,
+}).start();
 
-// FS LISTENER
-
-const fsListener = async () => {
-  const watcher = Deno.watchFs("./exercises");
-  let lastEventTs = performance.now();
-  for await (const event of watcher) {
-    const { kind, paths } = event;
-    if (kind === "modify") {
-      const eventTs = performance.now();
-      paths.forEach((path) => {
-        path = relative(Deno.cwd(), path);
-        const current = state.current();
-        if (current && current.path === path) {
-          if (eventTs - lastEventTs > 1000) { // throttling
-            lastEventTs = eventTs;
-            window.dispatchEvent(new Event(FileModifiedEvent));
-          }
-        }
-      });
-    }
-  }
-};
-
-fsListener();
+await tslings.rewind();
+tslings.checkExercise();
 
 export {};
